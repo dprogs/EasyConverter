8f1ca3b53ae5a9bf185973e0ce6ef62cc1569fa6
Dmitriy Levin <dprogs@gmail.com>
2014-07-20 19:08:08 +0400

    Change traget android version, move some comments from var_const

--- src/com/dprogs/EasyConverter/var_const.java
+++ src/com/dprogs/EasyConverter/var_const.java
@@ -23,7 +23,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 //0202FF
 public class var_const 
